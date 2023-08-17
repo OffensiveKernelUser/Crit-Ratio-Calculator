85c558a658abb88b8a2da05b47e9e2ee8b84e53e
KernelBreaker_ <103178752+OffensiveKernelUser@users.noreply.github.com>
2023-08-17 12:10:48 +0000

    Added CV calculation

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ totalDamage = 0
 rate = int(input("Crit Rate: "))
 damage = int(input("Crit Damage: "))
 
-maxRate = rate + damage / 2
-maxDamage = rate * 2 + damage
-print(f"Max Crit Rate: {maxRate}\nMax Crit Damage: {maxDamage}")
+print(f"Crit Value: {2 * rate + damage}")
 
 print(f"1000 ATK Default Crit DMG: {1000 + defaultAtk * rate * 0.01 * damage * 0.01}")
